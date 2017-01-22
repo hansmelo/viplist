04add3402b4cd6066f8bcf34763b259efda386a2
Hans Melo <hansmelo@gmail.com>
2017-01-22 23:37:25 +0000

    First page integration controller and view

--- src/main/java/com/hans/viplist/Configuration.java
+++ src/main/java/com/hans/viplist/Configuration.java
@@ -3,18 +3,10 @@ package com.hans.viplist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 @Controller
 public class Configuration {
-	
-	@RequestMapping("/")
-	@ResponseBody
-	public String hello() {
-		return "Hello, You're welcome the system Vips List";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Configuration.class, args);
