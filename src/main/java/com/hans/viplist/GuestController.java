ceb8434dab5ad0bef2954e38b06e71f59ec89d92
Hans Melo <hansmelo@gmail.com>
2017-01-30 23:10:56 +0000

    Integration with database

--- src/main/java/com/hans/viplist/GuestController.java
+++ src/main/java/com/hans/viplist/GuestController.java
@@ -1,13 +1,29 @@
 package com.hans.viplist;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.hans.viplist.model.Convidado;
+import com.hans.viplist.repository.ConvidadoRepository;
 
 @Controller
 public class GuestController {
 
+	@Autowired
+	private ConvidadoRepository repository;
+	
 	@RequestMapping("/")
 	public String index() {
 		return "index";
+	}
+	
+	@RequestMapping("listaconvidados")
+	public String listaConvidados(Model model) {
+		Iterable<Convidado> guests = repository.findAll();
+		model.addAttribute("guests", guests);
+		
+		return "listaconvidados";
 	}
 }
---
+++ src/main/java/com/hans/viplist/repository/ConvidadoRepository.java
@@ -0,0 +1,9 @@
+package com.hans.viplist.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.hans.viplist.model.Convidado;
+
+public interface ConvidadoRepository extends CrudRepository<Convidado, Long>{
+
+}
