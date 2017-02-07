f1b60a001569e89a61063e143e612cf1877ef6a9
Hans Melo <hansmelo@gmail.com>
2017-02-07 22:42:25 +0000

    Create file of configurations

--- src/main/java/com/hans/viplist/Configuration.java
+++ src/main/java/com/hans/viplist/Configuration.java
@@ -1,11 +1,7 @@
 package com.hans.viplist;
 
-import javax.sql.DataSource;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
@@ -14,14 +10,5 @@ public class Configuration {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Configuration.class, args);
-	}
-	
-	@Bean
-	public DataSource dataSource() {
-	    DriverManagerDataSource dataSource = new DriverManagerDataSource();
-	    dataSource.setUrl("jdbc:mysql://localhost:3306/listavip");
-	    dataSource.setUsername("root");
-	    dataSource.setPassword("");
-	    return dataSource;
 	}
 }
